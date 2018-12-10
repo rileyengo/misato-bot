74be472575f0554123c7995d68bf19f7e93aa60b
Americhino <rileyengo@gmail.com>
2018-12-09 18:58:30 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='?')
+TOKEN = environ['TOKEN']
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+bot.run(TOKEN)
