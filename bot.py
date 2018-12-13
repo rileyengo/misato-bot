72087c34e6b112cddf21abfb897dde936a9a464a
Americhino <rileyengo@gmail.com>
2018-12-12 22:15:59 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    client.change_presence(game=discord.Game(name='NERV HQ'), status=None, afk=False)
+    client.change_presence(game=discord.Game(name='NERV HQ'))
 
 ############################
 ### ?greet - hello there ###
