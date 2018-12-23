f058eb28a08592c09165f362a514c488475d61cc
Americhino <rileyengo@gmail.com>
2018-12-22 22:26:08 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,20 +21,20 @@ async def on_ready():
 ### LOAD AND UNLOAD EXTENSIONS ###
 ###########################
 @bot.command()
-async def load(extension_name : str):
+async def load(ctx, extension_name : str):
     """Loads an extension."""
     try:
-        bot.load_extension(extension_name)
+        ctx.load_extension(extension_name)
     except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        await ctx.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
         return
-    await bot.say("{} loaded.".format(extension_name))
+    await ctx.say("{} loaded.".format(extension_name))
 
 @bot.command()
-async def unload(extension_name : str):
+async def unload(ctx, extension_name : str):
     """Unloads an extension."""
-    bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
+    ctx.unload_extension(extension_name)
+    await ctx.say("{} unloaded.".format(extension_name))
 
 ###########################
 ### ?dice - Roll a dice ###
