00aa8e29663a12cd1a88f08c693c9f27843967da
Americhino <rileyengo@gmail.com>
2018-12-23 19:38:20 -0700

    Create general.py

---
+++ cogs/general.py
@@ -0,0 +1,39 @@
+from discord.ext import commands
+
+class GeneralCog:
+	def __init__(self, bot):
+		self.bot = bot
+	@bot.command()
+	async def load(extension_name : str):
+		"""Loads an extension."""
+		try:
+			bot.load_extension(extension_name)
+		except (AttributeError, ImportError) as e:
+			await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+			return
+		await bot.say("{} loaded.".format(extension_name))
+
+	@bot.command()
+	async def unload(extension_name : str):
+		"""Unloads an extension."""
+		bot.unload_extension(extension_name)
+		await bot.say("{} unloaded.".format(extension_name))
+
+	###########################
+	### ?dice - Roll a dice ###
+	###########################
+
+	@bot.command()
+	async def roll(dice : str):
+		"""Rolls a dice in NdN format."""
+		try:
+			rolls, limit = map(int, dice.split('d'))
+		except Exception:
+			await bot.say('Format has to be in NdN!')
+			return
+
+		result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+		await bot.send(':dice: ' + result)
+
+def setup(bot):
+    bot.add_cog(GeneralCog(bot))
