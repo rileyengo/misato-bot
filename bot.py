de29a32e9e677360546b30f561b129216ca0005e
Americhino <rileyengo@gmail.com>
2018-12-23 19:44:10 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from os import environ
 
 bot = commands.Bot(command_prefix='?')
 TOKEN = os.environ['TOKEN']
-startup_extensions = ["cogs.characters"]
+startup_extensions = ['cogs.characters', 'cogs.general']
 client = discord.Client()
 
 @bot.event
@@ -28,36 +28,4 @@ if __name__ == '__main__':
         except Exception as e:
             print(f'Failed to load extension {extension}.', file=sys.stderr)
             traceback.print_exc()
-@bot.command()
-async def load(extension_name : str):
-    """Loads an extension."""
-    try:
-        bot.load_extension(extension_name)
-    except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-        return
-    await bot.say("{} loaded.".format(extension_name))
-
-@bot.command()
-async def unload(extension_name : str):
-    """Unloads an extension."""
-    bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
-
-###########################
-### ?dice - Roll a dice ###
-###########################
-
-@bot.command()
-async def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.send(':dice: ' + result)
-  
-bot.run(TOKEN)
+bot.run(TOKEN, bot=True, reconnect=True)
