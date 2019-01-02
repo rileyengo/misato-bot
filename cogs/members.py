c0c94776f885ed73b4018e6c5630de4f8f5ef933
Americhino <rileyengo@gmail.com>
2019-01-01 22:25:05 -0700

    Update members.py

--- cogs/members.py
+++ cogs/members.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-class MembersCog:
+class Members:
     def __init__(self, bot):
         self.bot = bot
 
@@ -47,4 +47,4 @@ class MembersCog:
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
 # When we load the cog, we use the name of the file.
 def setup(bot):
-    bot.add_cog(MembersCog(bot))
+    bot.add_cog(Members(bot))
