1d92ced16027320517b7e3bb2ef20411175d3875
Americhino <rileyengo@gmail.com>
2019-01-01 22:29:47 -0700

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -20,22 +20,6 @@ class General:
 		bot.unload_extension(extension_name)
 		await bot.say("{} unloaded.".format(extension_name))
 
-	###########################
-	### ?dice - Roll a dice ###
-	###########################
-
-	@commands.command()
-	async def roll(self, ctx, dice : str):
-		"""Rolls a dice in NdN format."""
-		try:
-			rolls, limit = map(int, dice.split('d'))
-		except Exception:
-			await bot.say('Format has to be in NdN!')
-			return
-
-		result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-		await bot.say(result)
-		
 	############################
 	### ?greet - testing 123 ###
 	############################
