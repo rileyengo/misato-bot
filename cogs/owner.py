22cbf0a0cea8d0789b2a719414f77b528433681d
Americhino <rileyengo@gmail.com>
2019-01-02 22:38:48 -0700

    Create owner.py

---
+++ cogs/owner.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class Owner:
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(name='owner')
+	@commands.is_owner()
+		async def owner(self, ctx):
+		"""Only the owner can use this. Shh."""
+			await bot.say('You are an official affiliated with SEELE.')
+
+def setup(bot):
+    bot.add_cog(Owner(bot))
