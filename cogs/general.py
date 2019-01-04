c067dc08cf41afb46059a1e69cfb5c0b588562c9
Americhino <rileyengo@gmail.com>
2019-01-03 22:42:29 -0700

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -6,26 +6,26 @@ class General:
 		self.bot = bot
 	@commands.command(name='load')
 	@commands.is_owner()
-	async def load(extension_name : str):
+	async def load(ctx, extension_name : str):
 		"""Loads an extension."""
 		try:
 			bot.load_extension(extension_name)
 		except (AttributeError, ImportError) as e:
-			await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+			await ctx.send("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
 			return
 		await bot.say("{} loaded.".format(extension_name))
 
 	@commands.command(name='unload')
 	@commands.is_owner()
-	async def unload(extension_name : str):
+	async def unload(ctx, extension_name : str):
 		"""Unloads an extension."""
 		bot.unload_extension(extension_name)
-		await bot.say("{} unloaded.".format(extension_name))
+		await ctx.send("{} unloaded.".format(extension_name))
 
 	@commands.command(name='ping')
 	async def ping(self, ctx):
 		"""Ping!"""
-		await bot.say('Pong!')
+		await ctx.send('Pong!')
 		
 	@commands.command(name='repeat')
 	async def repeat(ctx, *, arg):
