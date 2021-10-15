3ceb29cccb5ea162302496d76b8a2cd9f2975389
Americhino <rileyengo@gmail.com>
2021-10-14 22:36:52 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="?")
 TOKEN = os.getenv("TOKEN")
-startup_extensions = ['general']
+startup_extensions = ['cogs.general']
 client = discord.Client()
 
 @bot.event
